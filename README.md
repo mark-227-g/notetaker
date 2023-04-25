7fb3fbdfa1fa964e223d96265fae2fa502517191
Mark Edwards <109709559+mark-227-g@users.noreply.github.com>
2023-04-24 23:10:33 -0400

    everything is working and readme is complete

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # notetaker
+
 Challenge 11
+
+## Author
+
+Mark Edwards
+
+## Deployment
+
+copy entire folder onto server
+run
+
+* npm install
+* npm start
+
+in browser navigate to
+
+```http://localhost:3001/```
+
+## Usage
+
+Follow instruction in acceptence criteria
+
+### Screenshots
+  
+![screenshot 1](./images/screenshot1.png)
+![screenshot 2](./images/screenshot2.png)
+
+## User Story
+
+```md
+AS A small business owner
+I WANT to be able to write and save notes
+SO THAT I can organize my thoughts and keep track of tasks I need to complete
+```
+
+## Acceptance Criteria
+
+```md
+GIVEN a note-taking application
+WHEN I open the Note Taker
+THEN I am presented with a landing page with a link to a notes page
+WHEN I click on the link to the notes page
+THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+WHEN I enter a new note title and the note’s text
+THEN a Save icon appears in the navigation at the top of the page
+WHEN I click on the Save icon
+THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+WHEN I click on an existing note in the list in the left-hand column
+THEN that note appears in the right-hand column
+WHEN I click on the Write icon in the navigation at the top of the page
+THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
+```
+
+## Technology used
+
+* expressjs
+* node.js
+* file library
+* SVG
+* npm
+* CU Week "11 - Express" for sample code
+
+## References
+
+* [node.js documentation](https://nodejs.org/dist/latest-v19.x/docs/api/)
+* [npm documentation](https://docs.npmjs.com)
+* [expressjs](https://expressjs.com)
+  
--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const { readAndAppend } = require('./helpers/fsUtils');
 const { readFromFile } = require('./helpers/fsUtils');
 const { writeToFile} = require('./helpers/fsUtils');
 
-// load notes for first time
+/****************************************
+ load notes for first time
+ ****************************************/
 readFromFile('./db/db.json').then((data) => { 
   var notes=JSON.parse(data)
 })
-
-// = require('./db/notes');
 
 const PORT = process.env.PORT || 3001;
 
@@ -22,26 +22,38 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+/****************************************
+ Route get '/'
+ Default load of index.html
+ ****************************************/
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+/****************************************
+Route get '/notes'
+ ****************************************/
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// GET request for notes
+/****************************************
+ route get '/api/notes'
+ returns all notes
+ ****************************************/
 app.get('/api/notes', (req, res) => 
-readFromFile('./db/db.json').then((data) => { 
-  notes=JSON.parse(data)
-  res.json(notes)
-})
+  readFromFile('./db/db.json').then((data) => { 
+    notes=JSON.parse(data)
+    res.json(notes)
+  })
 );
 
-// GET a single note
+/****************************************
+ route get '/api/notes/:id
+ returns one not with the id
+ ****************************************/
 app.get('/api/notes/:id', (req, res) => {
   if (req.params.id) {
-    console.info(`${req.method} request received to get a single a note`);
     const noteId = req.params.id;
     for (let i = 0; i < notes.length; i++) {
       const currentnote = notes[i];
@@ -56,14 +68,13 @@ app.get('/api/notes/:id', (req, res) => {
   }
 });
 
-// POST request to add a note
+/****************************************
+ route post '/api/notes'
+ add a new note
+ ****************************************/
 app.post('/api/notes', (req, res) => {
-  // Log that a POST request was received
-  console.info(`${req.method} request received to add a note`);
-
   // Destructuring assignment for the items in req.body
   const { title, text } = req.body;
-
   // If all the required properties are present
   if (title && text) {
     // Variable for the object we will save
@@ -77,78 +88,35 @@ app.post('/api/notes', (req, res) => {
       status: 'success',
       body: newnote,
     };
-
-    console.log(response);
     res.status(201).json(response);
   } else {
     res.status(500).json('Error in posting note');
   }
 });
 
-// GET request for a specific note's noteupvotes
-/*
-app.delete('/api/notnoteupvotese/:id', (req, res) => {
-  console.info(`${req.method} request received to get noteupvotes for a note`);
-  for (let i = 0; i < notes.length; i++) {
-    const currentnote = notes[i];
-    if (currentnote.id === req.params.id) {
-      res.status(200).json({
-        message: `The note with ID ${currentnote.id} has ${currentnote.noteupvotes}`,
-        noteupvotes: currentnote.noteupvotes,
-      });
-      return;
-    }
-  }
-  res.status(404).json('note ID not found');
-});*/
-
-// code from update
+/****************************************
+ route delete '/api/notes/:id
+ deletes one note with id
+ ****************************************/
 app.delete('/api/notes/:id', (req, res) => {
-  
-  console.info(`${req.method} request received to delete a single a note`);
   const noteId = req.params.id;
-  console.info(`noteId: ${noteId}`);
- // console.log(notes);
-  for (let i = 0; i < notes.length; i++) {
-    const currentnote = notes[i];
-    if (currentnote.id === noteId) {
-      const deletedNote=notes.splice(i,1)
-      console.log(notes)
-      writeToFile('./db/db.json',notes)
-
-      res.json(deletedNote);
-
-      return;
-    }
-   }
-   res.status(404).send('note not found');
-  }
-  //
-//} else {
- // res.status(400).send('note ID not provided');
-//}
-//}
-);
-// code from update
-
-// POST request to upvote a note
-/*
-app.delete('/api/noteupvotes/:id', (req, res) => {
-  if (req.body && req.params.id) {
-    console.info(`${req.method} request received to upvote a note`);
-    const noteId = req.params.id;
+  if(noteId){
     for (let i = 0; i < notes.length; i++) {
       const currentnote = notes[i];
       if (currentnote.id === noteId) {
-        currentnote.noteupvotes += 1;
-        res.status(200).json(`New upvote count is: ${currentnote.noteupvotes}!`);
+        const deletedNote=notes.splice(i,1)
+        writeToFile('./db/db.json',notes)
+        res.json(deletedNote);
         return;
       }
     }
-    res.status(404).json('note ID not found');
+    res.status(404).send('note not found');
+  }
+  else {
+    res.status(400).send('note ID not provided');
   }
 });
-*/
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
